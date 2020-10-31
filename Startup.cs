678407d1dba44df5bede32a97a8ad3617465670f
pechavarriaa <pechavarriaa@gmail.com>
2020-10-30 23:35:21 -0600

    added cors

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,12 +21,25 @@ namespace backend_learning_algorithm
             Configuration = configuration;
         }
 
+        private readonly string AllowSpecificOriginsCorsPolicy = "AllowSpecificOriginsCorsPolicy";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOriginsCorsPolicy",
+                builder =>
+                {
+                    var allowedOrigins = Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+                    builder
+                    .WithOrigins(allowedOrigins)
+                    .WithMethods("PUT", "DELETE", "GET", "POST", "OPTIONS")
+                    .WithHeaders(HeaderNames.ContentType);
+                });
+            });
             services.AddSwaggerGen();
         }
 
@@ -42,13 +56,10 @@ namespace backend_learning_algorithm
                 });
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseCors(AllowSpecificOriginsCorsPolicy);
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
