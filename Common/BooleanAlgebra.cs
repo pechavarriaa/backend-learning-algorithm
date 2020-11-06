4c7d1c37c59e3890b746b4f2649b2b5217006001
pechavarriaa <pechavarriaa@gmail.com>
2020-11-06 10:50:49 -0600

    added method to get single inverse relation

--- Common/BooleanAlgebra.cs
+++ Common/BooleanAlgebra.cs
@@ -6,14 +6,23 @@ namespace backend_learning_algorithm.Common
         public static Dictionary<BooleanAlgebraRelations, BooleanAlgebraRelations> GetInverseRelations()
         {
             return new Dictionary<BooleanAlgebraRelations, BooleanAlgebraRelations>(){
-                {BooleanAlgebraRelations.Contains, BooleanAlgebraRelations.During},
-                {BooleanAlgebraRelations.During, BooleanAlgebraRelations.Contains},
-                {BooleanAlgebraRelations.Finishes, BooleanAlgebraRelations.Finished_by},
-                {BooleanAlgebraRelations.Finished_by, BooleanAlgebraRelations.Finishes},
-                {BooleanAlgebraRelations.Start, BooleanAlgebraRelations.Started_by},
-                {BooleanAlgebraRelations.Started_by, BooleanAlgebraRelations.Start},
-                {BooleanAlgebraRelations.Equal, BooleanAlgebraRelations.Equal}
+                { BooleanAlgebraRelations.Contains, BooleanAlgebraRelations.During },
+                { BooleanAlgebraRelations.During, BooleanAlgebraRelations.Contains },
+                { BooleanAlgebraRelations.Finishes, BooleanAlgebraRelations.Finished_by },
+                { BooleanAlgebraRelations.Finished_by, BooleanAlgebraRelations.Finishes },
+                { BooleanAlgebraRelations.Start, BooleanAlgebraRelations.Started_by },
+                { BooleanAlgebraRelations.Started_by, BooleanAlgebraRelations.Start },
+                { BooleanAlgebraRelations.Equal, BooleanAlgebraRelations.Equal }
             };
+        }
+
+        public static BooleanAlgebraRelations GetInverseRelation(BooleanAlgebraRelations booleanAlgebraRelation)
+        {
+            var inverseRelations = BooleanAlgebra.GetInverseRelations();
+
+            return inverseRelations.ContainsKey(booleanAlgebraRelation) ?
+                   inverseRelations[booleanAlgebraRelation] :
+                   BooleanAlgebraRelations.None;
         }
     }
 }
