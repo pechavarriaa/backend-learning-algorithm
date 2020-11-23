7ec0ca7235aa8a96229fe8ad5dd382f16270a1ca
pechavarriaa <pechavarriaa@gmail.com>
2020-11-22 21:59:54 -0600

    defined relationship

---
+++ Contracts/Relationship.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using backend_learning_algorithm.Validation;
+
+namespace backend_learning_algorithm.Contracts
+{
+    public class Relationship
+    {
+        public string FirstVar { get; set; }
+
+        public string SecondVar { get; set; }
+
+        [RelationValidation(ErrorMessage="Invalid relation string")]
+        public List<string> Relations { get; set; }
+
+        public Relationship() { }
+
+        public Relationship(string firstVar, string secondVar, List<string> relations)
+        {
+            this.FirstVar = firstVar;
+            this.SecondVar = SecondVar;
+            this.Relations = relations;
+        }
+    }
+}
+   
